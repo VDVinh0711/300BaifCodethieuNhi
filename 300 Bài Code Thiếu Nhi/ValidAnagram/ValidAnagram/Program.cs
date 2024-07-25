2345aaf79c7c18bab23e7771d2413bc45b1a51cb
VDVinh0711 <130390041+VDVinh0711@users.noreply.github.com>
2024-07-25 23:07:39 +0700

    IsAnagram

---
+++ 300 Bài Code Thiếu Nhi/ValidAnagram/ValidAnagram/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ValidAnagram
+{
+    internal class Program
+    {
+        public static bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length) return false;
+            Dictionary<char, int> cacheCount = new Dictionary<char, int>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (cacheCount.ContainsKey(s[i]))
+                {
+                    cacheCount[s[i]]++;
+                    continue;
+                }
+
+                cacheCount[s[i]] = 1;
+            }
+
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (cacheCount.ContainsKey(t[i]) && cacheCount[t[i]] > 0)
+                {
+                    cacheCount[t[i]]--;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+                
+            return true;
+        }
+        public static void Main(string[] args)
+        {
+            string s = "rat";
+            string t = "car";
+            Console.WriteLine(IsAnagram(s,t));
+        }
+    }
+}
