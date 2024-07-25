c100cf54d5db31dfe244124db21a443e85e7b120
VDVinh0711 <130390041+VDVinh0711@users.noreply.github.com>
2024-07-25 22:50:11 +0700

    [200First Unique Character in a String

---
+++ 300 Bài Code Thiếu Nhi/First Unique Character in a String/First Unique Character in a String/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace First_Unique_Character_in_a_String
+{
+    internal class Program
+    {
+        public static int FirstUniqChar(string s) {
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
+            for(int i=0;i<s.Length;i++)
+            {
+                if(charCount.ContainsKey(s[i]))
+                {
+                    charCount[s[i]]++;
+                    continue;
+                }
+                charCount[s[i]] = 1;
+            }
+            for(int i =0 ;i< s.Length;i++)
+            {
+                if(charCount[s[i]] == 1) 
+                {
+                    return i;
+                }
+            }
+            return -1;
+        
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(FirstUniqChar("leetcode"));
+            Console.WriteLine(FirstUniqChar("loveleetcode"));
+        }
+    }
+}
