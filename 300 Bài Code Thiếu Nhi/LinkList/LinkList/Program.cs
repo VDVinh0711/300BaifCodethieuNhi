94641cb266c147851bf6255216cd0f6bdc4c47ad
VDVinh0711 <130390041+VDVinh0711@users.noreply.github.com>
2024-08-20 18:46:12 +0700

    linkList

---
+++ 300 Bài Code Thiếu Nhi/LinkList/LinkList/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+
+namespace LinkList
+{
+    public class Node {
+        public int data;
+        public Node next;
+
+        public Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+
+    internal class Program
+    {
+        public static void PrintList(Node head)
+        {
+            while (head != null)
+            {
+                Console.Write(head.data + " -> ");
+                head = head.next;
+            }
+
+            Console.WriteLine("null");
+        }
+
+        public static Node DeleteNode(Node head, int target)
+        {
+
+            if (head.next == null) return null;
+            if (head.data == target)
+            {
+                head = head.next;
+                return head;
+            }
+
+            Node current = head;
+            while (current != null)
+            {
+                if (current.next.data == target)
+                {
+                    current.next = current.next.next;
+                }
+
+                current = current.next;
+            }
+
+            return head;
+        }
+
+
+        public static Node DeleteNodeFollowIndex(Node head, int index)
+        {
+            if (head == null) return null;
+
+
+            Node cur = head;
+            Node temp = head;
+            int lengList = 0;
+            while (cur != null)
+            {
+                lengList++;
+                cur = cur.next;
+            }
+
+            int indexDelete = lengList - index;
+            if (indexDelete == 0)
+            {
+                head = head.next;
+                return head;
+            }
+
+            int j = 1;
+            while (temp != null)
+            {
+
+                if (j == indexDelete)
+                {
+                    temp.next = temp.next.next;
+                }
+
+                j++;
+                temp = temp.next;
+            }
+
+            return head;
+
+
+        }
+
+
+
+        public static Node ReverseList(Node head)
+        {
+            Node preNode = null;
+            Node cur = head;
+            while (cur != null)
+            {
+                Node nextNode = cur.next;
+                cur.next = preNode;
+                preNode = cur;
+                cur = nextNode;
+                
+                Console.WriteLine(preNode.data);
+            }
+
+            return preNode;
+        }
+    
+
+    public static void Main(string[] args)
+        {
+            Node head = new Node(1);
+             head.next = new Node(2);
+            head.next.next = new Node(3);
+            head.next.next.next = new Node(4);
+            head.next.next.next.next = new Node(5);
+            Console.WriteLine("Original list: ");
+            PrintList(head);
+            var nodeRevers = ReverseList(head);
+            PrintList(nodeRevers);
+
+
+        }
+    }
+}
---
+++ 300 Bài Code Thiếu Nhi/StrStr/StrStr/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace StrStr
+{
+    internal class Program
+    {
+        
+        
+        public static int StrStr(string haystack, string needle)
+        {
+            if (haystack == string.Empty || needle == string.Empty) return -1;
+            int result = -1;
+            if (haystack.Length < needle.Length) return -1;
+            
+            for (int i = 0; i <= haystack.Length - needle.Length; i++)
+            {
+                if (haystack[i] == needle[0])
+                {
+                    int startindex = i;
+                    int indexIndeel = 0;
+                    while (indexIndeel < needle.Length)
+                    {
+                        if (haystack[startindex] == needle[indexIndeel])
+                        {
+                            startindex++;
+                            indexIndeel++;
+                            result = i;
+                        }
+                        else
+                        { 
+                            result = -1;
+                           break;
+                        }
+                    }
+
+                   
+                }
+            }
+            return result;
+        }
+        public static void Main(string[] args)
+        {
+            Console.Write(StrStr("sadbutsad","sad"));
+        }
+    }
+}
