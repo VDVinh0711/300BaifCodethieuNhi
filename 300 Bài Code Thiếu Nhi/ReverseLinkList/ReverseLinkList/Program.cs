2759971432cf5a88cf632760344e0f1e4a688777
VDVinh0711 <130390041+VDVinh0711@users.noreply.github.com>
2024-08-21 23:26:57 +0700

    revertlinklist

---
+++ 300 Bài Code Thiếu Nhi/ReverseLinkList/ReverseLinkList/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ReverseLinkList
+{
+  
+  public class Node {
+    public int data;
+    public Node next;
+
+    public Node(int data)
+    {
+      this.data = data;
+      this.next = null;
+    }
+  }
+  
+  
+  
+  internal class Program
+  {
+    
+    public static Node ReverseList(Node head)
+    {
+      Node preNode = null;
+      Node cur = head;
+      while (cur != null)
+      {
+        Node nextNode = cur.next;
+        cur.next = preNode;
+        preNode = cur;
+        cur = nextNode;
+         
+        Console.WriteLine(preNode.data);
+      }
+
+      return preNode;
+    }
+    
+    
+    public static void PrintList(Node head)
+    {
+      while (head != null)
+      {
+        Console.Write(head.data + " -> ");
+        head = head.next;
+      }
+
+      Console.WriteLine("null");
+    }
+    public static void Main(string[] args)
+    {
+      Node head = new Node(1);
+      head.next = new Node(2);
+      head.next.next = new Node(3);
+      head.next.next.next = new Node(4);
+      head.next.next.next.next = new Node(5);
+      Console.WriteLine("Original list: ");
+      PrintList(head);
+      var nodeRevers = ReverseList(head);
+      PrintList(nodeRevers);
+    }
+  }
+}
