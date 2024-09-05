d76eb8be7a0fb853cb8d2c0cb932be57c6b90d97
VDVinh0711 <130390041+VDVinh0711@users.noreply.github.com>
2024-09-05 22:23:27 +0700

    threesome

---
+++ 300 Bài Code Thiếu Nhi/CountPrimes/CountPrimes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CountPrimes
+{
+    internal class Program
+    {
+        
+        public static bool IsPrimes(int n)
+        {
+            if(n <2) return false;
+            int count = 0;
+            for(int i=1;i<= Math.Sqrt(n);i++)
+            {
+                if(n%i ==0)
+                {
+                    count++;
+                }
+                
+            }
+            return count <2 ? true : false;
+        }
+        public static void Main(string[] args)
+        {
+            
+            Console.Write(IsPrimes(4));
+        }
+    }
+}
---
+++ 300 Bài Code Thiếu Nhi/PowerOfThreee/PowerOfThreee/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PowerOfThreee
+{
+    internal class Program
+    {
+        
+        public static bool IsPowerOfThree(int n) {
+            if (n <= 0) return false;
+            double logResult = Math.Log10(n) / Math.Log10(3);
+            return Math.Abs(logResult - Math.Round(logResult)) < 1e-10;
+
+            
+            
+            
+        }
+        
+       
+        public static void Main(string[] args)
+        {
+        
+            
+            
+        }
+    }
+}
