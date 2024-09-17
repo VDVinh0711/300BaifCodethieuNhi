f6ca1ee698a42ddeff7b209b33c8b39421d7fc23
VDVinh0711 <130390041+VDVinh0711@users.noreply.github.com>
2024-09-18 00:01:55 +0700

    isvalidbst

--- 300 Bài Code Thiếu Nhi/Trees/Trees/Program.cs
+++ 300 Bài Code Thiếu Nhi/Trees/Trees/Program.cs
@@ -40,17 +40,38 @@ namespace Trees
                 return rightLength += 1;
             }
         }
+
+
+
+        public static bool IsValidBST(TreeNode root)
+        {
+            return IsValidBST(root,long.MinValue, long.MaxValue);
+        }
+
+        public static bool IsValidBST(TreeNode root, long minvalue, long maxvalue)
+        {
+            if (root == null) return true;
+            if (root.val >= maxvalue || root.val <= minvalue) return false;
+            return IsValidBST(root.left, minvalue, root.val) && IsValidBST(root.right, root.val, maxvalue);
+        }
+        
         
         public static void Main(string[] args)
         {
-            TreeNode root = new TreeNode(-8);
-            root.left = new TreeNode(9);
-            root.right = new TreeNode(20);
-            root.right.left = new TreeNode(15);
-            root.right.right = new TreeNode(7);
-            ShowTrees(root);
+            TreeNode root = new TreeNode(2);
+            root.left = new TreeNode(1);
+            root.right = new TreeNode(3);
+
+
+            TreeNode root2 = new TreeNode(5);
+            root2.left = new TreeNode(1);
+            root2.right = new TreeNode(4);
+            root2.right.left = new TreeNode(3);
+            root2.right.right = new TreeNode(6);
+         
             
-            Console.WriteLine("DeepLegnth " +  MaxDepth( root));
+            Console.WriteLine("Result " +  IsValidBST( root));
+            Console.WriteLine("Result2" + IsValidBST(root2));
         }
     }
 }
