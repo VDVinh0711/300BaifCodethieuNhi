4003469024776d6af6c9e3d36574c86f05efe081
VDVinh0711 <130390041+VDVinh0711@users.noreply.github.com>
2024-09-20 13:17:01 +0700

    ddb

---
+++ 300 Bài Code Thiếu Nhi/DoanCon/DoanCon/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DoanCon
+{
+    internal class Program
+    {   
+        public static int CountSubArrayEqualZero(int[] nums)
+        {
+            int result = 0;
+            for (int i = 0; i < nums.Length-1; i++)
+            {
+                int sum = 0;
+                for (int j = i; j < nums.Length ; j++)
+                {
+                    sum += nums[j];
+                    if (sum == 0)
+                    {
+                        result++;
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+        public static void Main(string[] args)
+        {
+            int[] input = new int[] { -3,1,0,-1,3};
+            Console.WriteLine(CountSubArrayEqualZero(input));
+        }
+    }
+}
---
+++ 300 Bài Code Thiếu Nhi/ThreeSome/ThreeSome/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Stopwatch stopwatch = new Stopwatch();
+
+        // Bắt đầu đo thời gian
+        stopwatch.Start();
+        int[] sequence = new int[] {1, 4, 3, 2, 1, 2, 3, 4, 1};
+        Console.WriteLine(CalculateInsertions(sequence));
+        
+        stopwatch.Stop();
+
+        // In ra thời gian đã chạy
+        Console.WriteLine($"Thời gian chạy: {stopwatch.ElapsedMilliseconds} ms");
+    }
+
+    static int CalculateInsertions(int[] seq)
+    {
+        int left = 0, right = seq.Length - 1, insertions = 0;
+        while (left < right)
+        {
+            if (seq[left] == seq[right]) { left++; right--; }
+            else
+            {
+                if (seq[left] == seq[right - 1])
+                {
+                    insertions++;
+                    right--;
+                    
+                }
+                else if(seq[left+1] == seq[right])
+                {
+                    insertions++;
+                    left++;
+                }
+                else
+                {
+                    insertions += 2;
+                    left++;
+                    right--;
+                }
+            }
+        }
+        return insertions;
+    }
+}
